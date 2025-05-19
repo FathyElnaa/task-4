58d526efd175942e2e1f2b51b7bdcd6f3c6cd5a4
fathy <fathyelnaa5@gmail.com>
2025-05-19 04:11:46 +0300

    task-4u

--- form_val.php
+++ form_val.php
@@ -39,8 +39,5 @@
             </form>
         </div>
     </div>
-
-    <!-- Bootstrap JS (اختياري لو هتستخدم وظائف ديناميكية) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
